8bf626d1ea01b07085f266f4cd2ebd5713e60ecd
SO Pulatov <SirotilloPulatov@gmail.com>
2021-10-10 17:47:11 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let menu = document.querySelector('.header__menu');
+let navbar = document.querySelector('.header__nav')
+
+
+menu.addEventListener("click", function () {
+  navbar.classList.toggle('nav-show');
+})
+
