2d858a7a894ac859d3256a5122cad198b7e39f04
SO Pulatov <SirotilloPulatov@gmail.com>
2021-10-11 14:34:12 +0500

    added sections

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 
 let menu = document.querySelector('.header__menu');
-let navbar = document.querySelector('.header__nav')
+let navbar = document.querySelector('.header__nav');
+let navList = document.querySelector('.nav-list')
 
 
 menu.addEventListener("click", function () {
   navbar.classList.toggle('nav-show');
+  navList.classList.toggle('nav-list__show')
 })
 
